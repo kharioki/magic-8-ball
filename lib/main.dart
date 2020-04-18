172122d65f70dd25b963a87099e08356df8002ef
kharioki <antolisah@gmail.com>
2020-04-18 21:57:15 +0300

    initial setup and dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          title: Text('Magic-8-Ball'),
+          backgroundColor: Colors.teal.shade600,
+        ),
+        body: Magic8Ball(),
       ),
-    );
+    ),
+  );
+}
+
+class Magic8Ball extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
